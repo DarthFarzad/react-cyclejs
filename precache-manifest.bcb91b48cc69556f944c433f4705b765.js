43c126006bfa96e7424dadccf6d9ce922a945b72
Farzad Slamdien <farzad@slamdien.co.za>
2020-07-16 03:52:13 +0200

    Updates

---
+++ precache-manifest.bcb91b48cc69556f944c433f4705b765.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0432c42b7d81467b6613d39cd71cdddd",
+    "url": "/react-cyclejs/index.html"
+  },
+  {
+    "revision": "bb5703fcf8edd6ca50c4",
+    "url": "/react-cyclejs/static/css/main.946f5e11.chunk.css"
+  },
+  {
+    "revision": "c1b366c7f9ab428f2910",
+    "url": "/react-cyclejs/static/js/2.51f3f9be.chunk.js"
+  },
+  {
+    "revision": "42dbb93dfc6795ee85db77d70b0089b9",
+    "url": "/react-cyclejs/static/js/2.51f3f9be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb5703fcf8edd6ca50c4",
+    "url": "/react-cyclejs/static/js/main.16091ea7.chunk.js"
+  },
+  {
+    "revision": "d63252d32d6026c12245",
+    "url": "/react-cyclejs/static/js/runtime-main.eb1a5aff.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-cyclejs/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "react-cyclejs/precache-manifest.234650824ba170f390c063eacabf0788.js"
+  "/react-cyclejs/precache-manifest.bcb91b48cc69556f944c433f4705b765.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("react-cyclejs/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-cyclejs/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
