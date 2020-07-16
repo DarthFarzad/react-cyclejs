ca5529040b9369db26401489d5aa2182ac8609d4
Farzad Slamdien <farzad@slamdien.co.za>
2020-07-16 03:27:31 +0200

    Updates

---
+++ precache-manifest.2e867987e225b2ff6cd99b386b6d829f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81bc7aed0247ddbaa68cb832a8d7c861",
+    "url": "/react-cyclejs/index.html"
+  },
+  {
+    "revision": "1a64eda816d83a248d32",
+    "url": "/react-cyclejs/static/css/main.946f5e11.chunk.css"
+  },
+  {
+    "revision": "c1b366c7f9ab428f2910",
+    "url": "/react-cyclejs/static/js/2.51f3f9be.chunk.js"
+  },
+  {
+    "revision": "42dbb93dfc6795ee85db77d70b0089b9",
+    "url": "/react-cyclejs/static/js/2.51f3f9be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a64eda816d83a248d32",
+    "url": "/react-cyclejs/static/js/main.406a6569.chunk.js"
+  },
+  {
+    "revision": "d63252d32d6026c12245",
+    "url": "/react-cyclejs/static/js/runtime-main.eb1a5aff.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-cyclejs/static/media/logo.5d5d9eef.svg"
+  }
+]);
